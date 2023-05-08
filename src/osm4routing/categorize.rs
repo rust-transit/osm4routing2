4ad74f32605c1f20700f438a73e6dcbec0de95db
Tristram Gräbener <tristramg@gmail.com>
2023-05-08 09:51:27 +0200

    Use enums not integers for values

--- src/osm4routing/categorize.rs
+++ src/osm4routing/categorize.rs
@@ -1,62 +1,60 @@
-// UNKNOWN accessiblity
-const UNKNOWN: i8 = -1;
+use serde::Serialize;
 
-// FOOT_FORBIDDEN that no pedestrian is allowed
-const FOOT_FORBIDDEN: i8 = 0;
-// FOOT_ALLOWED pedestrians are allowed in both directions
-const FOOT_ALLOWED: i8 = 1;
+#[derive(Clone, Copy, Debug, Serialize, PartialEq)]
+pub enum FootAccessibility {
+    Unknown,
+    Forbidden,
+    Allowed,
+}
+#[derive(Clone, Copy, Debug, Serialize, PartialEq)]
+pub enum CarAccessibility {
+    Unknown,
+    Forbidden,
+    Residential, // http://wiki.openstreetmap.org/wiki/Tag:highway%3Dresidential
+    Tertiary,    // http://wiki.openstreetmap.org/wiki/Tag:highway%3Dtertiary
+    Secondary,   // http://wiki.openstreetmap.org/wiki/Tag:highway%3Dsecondary
+    Primary,     // http://wiki.http://wiki.openstreetmap.org/wiki/Tag:highway%3Dprimary
+    Trunk,       // http://wiki.openstreetmap.org/wiki/Tag:highway%3Dtrunk
+    Motorway,    // http://wiki.openstreetmap.org/wiki/Tag:highway%3Dmotorway
+}
 
-// CAR_FORBIDDEN no car is allowed
-const CAR_FORBIDDEN: i8 = 0;
-// CAR_RESIDENTIAL http://wiki.openstreetmap.org/wiki/Tag:highway%3Dresidential
-const CAR_RESIDENTIAL: i8 = 1;
-// CAR_TERTIARY http://wiki.openstreetmap.org/wiki/Tag:highway%3Dtertiary
-const CAR_TERTIARY: i8 = 2;
-// CAR_SECONDARY http://wiki.openstreetmap.org/wiki/Tag:highway%3Dsecondary
-const CAR_SECONDARY: i8 = 3;
-// car_forward http://wiki.http://wiki.openstreetmap.org/wiki/Tag:highway%3Dprimary
-const CAR_PRIMARY: i8 = 4;
-// CAR_TRUNK http://wiki.openstreetmap.org/wiki/Tag:highway%3Dtrunk
-const CAR_TRUNK: i8 = 5;
-// CAR_MOTORWAY http://wiki.openstreetmap.org/wiki/Tag:highway%3Dmotorway
-const CAR_MOTORWAY: i8 = 6;
+#[derive(Clone, Copy, Debug, Serialize, PartialEq)]
+pub enum BikeAccessibility {
+    Unknown,
+    Forbidden,
+    Allowed, // can be used by a bike, but the traffic might be shared with a car
+    Lane,    // narrow lane dedicated for bikes, without physical separation from other traffic
+    Busway,  // bikes are allowed on the bus lane
+    Track,   // physically separated for any other traffic
+}
 
-// BIKE_FORBIDDEN BIKE_ can not use this edge
-const BIKE_FORBIDDEN: i8 = 0;
-// BIKE_ALLOWED means that it can be used by a BIKE_, but the traffic might be shared with CAR_s
-const BIKE_ALLOWED: i8 = 2;
-// BIKE_LANE is a narrow lane dedicated for BIKE_, without physical separation from other traffic
-const BIKE_LANE: i8 = 3;
-// BIKE_BUSWAY means that BIKE_s are allowed on the bus lane
-const BIKE_BUSWAY: i8 = 4;
-// BIKE_TRACK is a physically separated for any other traffic
-const BIKE_TRACK: i8 = 5;
-
-// TRAIN_FORBIDEN no train is allowed
-const TRAIN_FORBIDDEN: i8 = 0;
-// TRAIN_ALLOWED means there are rails
-const TRAIN_ALLOWED: i8 = 1;
+#[derive(Clone, Copy, Debug, Serialize, PartialEq)]
+pub enum TrainAccessibility {
+    Unknown,
+    Forbidden,
+    Allowed,
+}
 
 // Edgeself contains what mode can use the edge in each direction
 #[derive(Clone, Copy)]
 pub struct EdgeProperties {
-    pub foot: i8,
-    pub car_forward: i8,
-    pub car_backward: i8,
-    pub bike_forward: i8,
-    pub bike_backward: i8,
-    pub train: i8,
+    pub foot: FootAccessibility,
+    pub car_forward: CarAccessibility,
+    pub car_backward: CarAccessibility,
+    pub bike_forward: BikeAccessibility,
+    pub bike_backward: BikeAccessibility,
+    pub train: TrainAccessibility,
 }
 
 impl Default for EdgeProperties {
     fn default() -> EdgeProperties {
         EdgeProperties {
-            foot: UNKNOWN,
-            car_forward: UNKNOWN,
-            car_backward: UNKNOWN,
-            bike_forward: UNKNOWN,
-            bike_backward: UNKNOWN,
-            train: UNKNOWN,
+            foot: FootAccessibility::Unknown,
+            car_forward: CarAccessibility::Unknown,
+            car_backward: CarAccessibility::Unknown,
+            bike_forward: BikeAccessibility::Unknown,
+            bike_backward: BikeAccessibility::Unknown,
+            train: TrainAccessibility::Unknown,
         }
     }
 }
@@ -64,40 +62,40 @@ impl Default for EdgeProperties {
 impl EdgeProperties {
     // Normalize fills UNKNOWN fields
     pub fn normalize(&mut self) {
-        if self.car_backward == UNKNOWN {
+        if self.car_backward == CarAccessibility::Unknown {
             self.car_backward = self.car_forward;
         }
-        if self.bike_backward == UNKNOWN {
+        if self.bike_backward == BikeAccessibility::Unknown {
             self.bike_backward = self.bike_forward;
         }
-        if self.car_forward == UNKNOWN {
-            self.car_forward = CAR_FORBIDDEN;
+        if self.car_forward == CarAccessibility::Unknown {
+            self.car_forward = CarAccessibility::Forbidden;
         }
-        if self.bike_forward == UNKNOWN {
-            self.bike_forward = BIKE_FORBIDDEN;
+        if self.bike_forward == BikeAccessibility::Unknown {
+            self.bike_forward = BikeAccessibility::Forbidden;
         }
-        if self.car_backward == UNKNOWN {
-            self.car_backward = CAR_FORBIDDEN;
+        if self.car_backward == CarAccessibility::Unknown {
+            self.car_backward = CarAccessibility::Forbidden;
         }
-        if self.bike_backward == UNKNOWN {
-            self.bike_backward = BIKE_FORBIDDEN;
+        if self.bike_backward == BikeAccessibility::Unknown {
+            self.bike_backward = BikeAccessibility::Forbidden;
         }
-        if self.foot == UNKNOWN {
-            self.foot = FOOT_FORBIDDEN;
+        if self.foot == FootAccessibility::Unknown {
+            self.foot = FootAccessibility::Forbidden;
         }
-        if self.train == UNKNOWN {
-            self.train = TRAIN_FORBIDDEN;
+        if self.train == TrainAccessibility::Unknown {
+            self.train = TrainAccessibility::Forbidden;
         }
     }
 
     // Accessible means that at least one mean of transportation can use it in one direction
     pub fn accessible(self) -> bool {
-        self.bike_forward != BIKE_FORBIDDEN
-            || self.bike_backward != BIKE_FORBIDDEN
-            || self.car_forward != CAR_FORBIDDEN
-            || self.car_backward != CAR_FORBIDDEN
-            || self.foot != FOOT_FORBIDDEN
-            || self.train != TRAIN_FORBIDDEN
+        self.bike_forward != BikeAccessibility::Forbidden
+            || self.bike_backward != BikeAccessibility::Forbidden
+            || self.car_forward != CarAccessibility::Forbidden
+            || self.car_backward != CarAccessibility::Forbidden
+            || self.foot != FootAccessibility::Forbidden
+            || self.train != TrainAccessibility::Forbidden
     }
 
     pub fn update(&mut self, key_string: String, val_string: String) {
@@ -109,85 +107,91 @@ impl EdgeProperties {
     pub fn update_with_str(&mut self, key: &str, val: &str) {
         match key {
             "highway" => match val {
-                "cycleway" | "path" | "footway" | "steps" | "pedestrian" => {
-                    self.bike_forward = BIKE_TRACK;
-                    self.foot = FOOT_ALLOWED;
+                "cycleway" => {
+                    self.bike_forward = BikeAccessibility::Track;
+                    self.foot = FootAccessibility::Allowed;
+                }
+                "path" | "footway" | "steps" | "pedestrian" => {
+                    self.bike_forward = BikeAccessibility::Allowed;
+                    self.foot = FootAccessibility::Allowed;
                 }
                 "primary" | "primary_link" => {
-                    self.car_forward = CAR_PRIMARY;
-                    self.foot = FOOT_ALLOWED;
-                    self.bike_forward = BIKE_ALLOWED;
+                    self.car_forward = CarAccessibility::Primary;
+                    self.foot = FootAccessibility::Allowed;
+                    self.bike_forward = BikeAccessibility::Allowed;
                 }
                 "secondary" => {
-                    self.car_forward = CAR_SECONDARY;
-                    self.foot = FOOT_ALLOWED;
-                    self.bike_forward = BIKE_ALLOWED;
+                    self.car_forward = CarAccessibility::Secondary;
+                    self.foot = FootAccessibility::Allowed;
+                    self.bike_forward = BikeAccessibility::Allowed;
                 }
                 "tertiary" => {
-                    self.car_forward = CAR_TERTIARY;
-                    self.foot = FOOT_ALLOWED;
-                    self.bike_forward = BIKE_ALLOWED;
+                    self.car_forward = CarAccessibility::Tertiary;
+                    self.foot = FootAccessibility::Allowed;
+                    self.bike_forward = BikeAccessibility::Allowed;
                 }
                 "unclassified" | "residential" | "living_street" | "road" | "service" | "track" => {
-                    self.car_forward = CAR_RESIDENTIAL;
-                    self.foot = FOOT_ALLOWED;
-                    self.bike_forward = BIKE_ALLOWED;
+                    self.car_forward = CarAccessibility::Residential;
+                    self.foot = FootAccessibility::Allowed;
+                    self.bike_forward = BikeAccessibility::Allowed;
                 }
                 "motorway" | "motorway_link" => {
-                    self.car_forward = CAR_MOTORWAY;
-                    self.foot = FOOT_FORBIDDEN;
-                    self.bike_forward = BIKE_FORBIDDEN;
+                    self.car_forward = CarAccessibility::Motorway;
+                    self.foot = FootAccessibility::Forbidden;
+                    self.bike_forward = BikeAccessibility::Forbidden;
                 }
                 "trunk" | "trunk_link" => {
-                    self.car_forward = CAR_TRUNK;
-                    self.foot = FOOT_FORBIDDEN;
-                    self.bike_forward = BIKE_FORBIDDEN;
+                    self.car_forward = CarAccessibility::Trunk;
+                    self.foot = FootAccessibility::Forbidden;
+                    self.bike_forward = BikeAccessibility::Forbidden;
                 }
                 _ => {}
             },
             "pedestrian" | "foot" => match val {
-                "no" => self.foot = FOOT_FORBIDDEN,
-                _ => self.foot = FOOT_ALLOWED,
+                "no" => self.foot = FootAccessibility::Forbidden,
+                _ => self.foot = FootAccessibility::Allowed,
             },
 
             // http://wiki.openstreetmap.org/wiki/Cycleway
             // http://wiki.openstreetmap.org/wiki/Map_Features#Cycleway
             "cycleway" => match val {
-                "track" => self.bike_forward = BIKE_TRACK,
-                "opposite_track" => self.bike_backward = BIKE_TRACK,
-                "opposite" => self.bike_backward = BIKE_ALLOWED,
-                "share_busway" => self.bike_forward = BIKE_BUSWAY,
-                "lane_left" | "opposite_lane" => self.bike_backward = BIKE_LANE,
-                _ => self.bike_forward = BIKE_LANE,
+                "track" => self.bike_forward = BikeAccessibility::Track,
+                "opposite_track" => self.bike_backward = BikeAccessibility::Track,
+                "opposite" => self.bike_backward = BikeAccessibility::Allowed,
+                "share_busway" => self.bike_forward = BikeAccessibility::Busway,
+                "lane_left" | "opposite_lane" => self.bike_backward = BikeAccessibility::Lane,
+                _ => self.bike_forward = BikeAccessibility::Lane,
             },
 
             "bicycle" => match val {
-                "no" | "false" => self.bike_forward = BIKE_FORBIDDEN,
-                _ => self.bike_forward = BIKE_ALLOWED,
+                "no" | "false" => self.bike_forward = BikeAccessibility::Forbidden,
+                _ => self.bike_forward = BikeAccessibility::Allowed,
             },
             "busway" => match val {
-                "opposite_lane" | "opposite_track" => self.bike_backward = BIKE_BUSWAY,
-                _ => self.bike_forward = BIKE_BUSWAY,
+                "opposite_lane" | "opposite_track" => {
+                    self.bike_backward = BikeAccessibility::Busway
+                }
+                _ => self.bike_forward = BikeAccessibility::Busway,
             },
             "oneway" => match val {
                 "yes" | "true" | "1" => {
-                    self.car_backward = CAR_FORBIDDEN;
-                    if self.bike_backward == UNKNOWN {
-                        self.bike_backward = BIKE_FORBIDDEN;
+                    self.car_backward = CarAccessibility::Forbidden;
+                    if self.bike_backward == BikeAccessibility::Unknown {
+                        self.bike_backward = BikeAccessibility::Forbidden;
                     }
                 }
                 _ => {}
             },
             "junction" => {
                 if val == "roundabout" {
-                    self.car_backward = CAR_FORBIDDEN;
-                    if self.bike_backward == UNKNOWN {
-                        self.bike_backward = BIKE_FORBIDDEN;
+                    self.car_backward = CarAccessibility::Forbidden;
+                    if self.bike_backward == BikeAccessibility::Unknown {
+                        self.bike_backward = BikeAccessibility::Forbidden;
                     }
                 }
             }
             "railway" => {
-                self.train = TRAIN_ALLOWED;
+                self.train = TrainAccessibility::Allowed;
             }
             _ => {}
         }
@@ -200,102 +204,102 @@ fn test_accessible() {
     p.normalize();
     assert!(!p.accessible());
 
-    p.foot = FOOT_ALLOWED;
+    p.foot = FootAccessibility::Allowed;
     assert!(p.accessible())
 }
 
 #[test]
 fn test_normalize() {
     let mut p = EdgeProperties::default();
-    p.bike_forward = BIKE_LANE;
+    p.bike_forward = BikeAccessibility::Lane;
     p.normalize();
-    assert_eq!(BIKE_LANE, p.bike_backward);
-    p.bike_forward = BIKE_ALLOWED;
+    assert_eq!(BikeAccessibility::Lane, p.bike_backward);
+    p.bike_forward = BikeAccessibility::Allowed;
     p.normalize();
-    assert_eq!(BIKE_LANE, p.bike_backward);
+    assert_eq!(BikeAccessibility::Lane, p.bike_backward);
 
-    p.car_forward = CAR_SECONDARY;
-    p.car_backward = UNKNOWN;
+    p.car_forward = CarAccessibility::Secondary;
+    p.car_backward = CarAccessibility::Unknown;
     p.normalize();
-    assert_eq!(CAR_SECONDARY, p.car_backward)
+    assert_eq!(CarAccessibility::Secondary, p.car_backward)
 }
 
 #[test]
 fn test_update() {
     let mut p = EdgeProperties::default();
     p.update_with_str("highway", "secondary");
-    assert_eq!(CAR_SECONDARY, p.car_forward);
+    assert_eq!(CarAccessibility::Secondary, p.car_forward);
 
     p.update_with_str("highway", "primary_link");
-    assert_eq!(CAR_PRIMARY, p.car_forward);
+    assert_eq!(CarAccessibility::Primary, p.car_forward);
 
     p.update_with_str("highway", "motorway");
-    assert_eq!(CAR_MOTORWAY, p.car_forward);
+    assert_eq!(CarAccessibility::Motorway, p.car_forward);
 
     p.update_with_str("highway", "residential");
-    assert_eq!(CAR_RESIDENTIAL, p.car_forward);
+    assert_eq!(CarAccessibility::Residential, p.car_forward);
 
     p.update_with_str("highway", "tertiary");
-    assert_eq!(CAR_TERTIARY, p.car_forward);
+    assert_eq!(CarAccessibility::Tertiary, p.car_forward);
 
     p.update_with_str("highway", "trunk");
-    assert_eq!(CAR_TRUNK, p.car_forward);
+    assert_eq!(CarAccessibility::Trunk, p.car_forward);
 
     p.update_with_str("highway", "cycleway");
-    assert_eq!(BIKE_TRACK, p.bike_forward);
-    assert_eq!(FOOT_ALLOWED, p.foot);
+    assert_eq!(BikeAccessibility::Track, p.bike_forward);
+    assert_eq!(FootAccessibility::Allowed, p.foot);
 
     p.update_with_str("foot", "designated");
-    assert_eq!(FOOT_ALLOWED, p.foot);
+    assert_eq!(FootAccessibility::Allowed, p.foot);
 
     p.update_with_str("foot", "no");
-    assert_eq!(FOOT_FORBIDDEN, p.foot);
+    assert_eq!(FootAccessibility::Forbidden, p.foot);
 
     p.update_with_str("cycleway", "lane");
-    assert_eq!(BIKE_LANE, p.bike_forward);
+    assert_eq!(BikeAccessibility::Lane, p.bike_forward);
 
     p.update_with_str("cycleway", "track");
-    assert_eq!(BIKE_TRACK, p.bike_forward);
+    assert_eq!(BikeAccessibility::Track, p.bike_forward);
 
     p.update_with_str("cycleway", "opposite_lane");
-    assert_eq!(BIKE_LANE, p.bike_backward);
+    assert_eq!(BikeAccessibility::Lane, p.bike_backward);
 
     p.update_with_str("cycleway", "opposite_track");
-    assert_eq!(BIKE_TRACK, p.bike_backward);
+    assert_eq!(BikeAccessibility::Track, p.bike_backward);
 
     p.update_with_str("cycleway", "opposite");
-    assert_eq!(BIKE_ALLOWED, p.bike_backward);
+    assert_eq!(BikeAccessibility::Allowed, p.bike_backward);
 
     p.update_with_str("cycleway", "share_busway");
-    assert_eq!(BIKE_BUSWAY, p.bike_forward);
+    assert_eq!(BikeAccessibility::Busway, p.bike_forward);
 
     p.update_with_str("cycleway", "lane_left");
-    assert_eq!(BIKE_LANE, p.bike_backward);
+    assert_eq!(BikeAccessibility::Lane, p.bike_backward);
 
     p.update_with_str("bicycle", "yes");
-    assert_eq!(BIKE_ALLOWED, p.bike_forward);
+    assert_eq!(BikeAccessibility::Allowed, p.bike_forward);
 
     p.update_with_str("bicycle", "no");
-    assert_eq!(BIKE_FORBIDDEN, p.bike_forward);
+    assert_eq!(BikeAccessibility::Forbidden, p.bike_forward);
 
     p.update_with_str("busway", "yes");
-    assert_eq!(BIKE_BUSWAY, p.bike_forward);
+    assert_eq!(BikeAccessibility::Busway, p.bike_forward);
 
     p.update_with_str("busway", "opposite_track");
-    assert_eq!(BIKE_BUSWAY, p.bike_backward);
+    assert_eq!(BikeAccessibility::Busway, p.bike_backward);
 
     p.update_with_str("oneway", "yes");
-    assert_eq!(BIKE_FORBIDDEN, p.car_backward);
-    assert!(p.bike_backward != BIKE_FORBIDDEN);
+    assert_eq!(CarAccessibility::Forbidden, p.car_backward);
+    assert!(p.bike_backward != BikeAccessibility::Forbidden);
 
-    p.bike_backward = UNKNOWN;
+    p.bike_backward = BikeAccessibility::Unknown;
     p.update_with_str("oneway", "yes");
-    assert_eq!(BIKE_FORBIDDEN, p.bike_backward);
+    assert_eq!(BikeAccessibility::Forbidden, p.bike_backward);
 
     p.update_with_str("junction", "roundabout");
-    assert_eq!(BIKE_FORBIDDEN, p.car_backward);
+    assert_eq!(CarAccessibility::Forbidden, p.car_backward);
 
-    p.bike_backward = UNKNOWN;
+    p.bike_backward = BikeAccessibility::Unknown;
     p.update_with_str("junction", "roundabout");
-    assert_eq!(BIKE_FORBIDDEN, p.bike_backward);
+    assert_eq!(BikeAccessibility::Forbidden, p.bike_backward);
 }
