7d0922b1351440b3a1f74923a8558095e2d057f2
Capucinoxx <coub2802@usherbrooke.ca>
2023-06-28 11:28:05 -0400

    chore: add args support for csv reader

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,36 @@
-fn main() {
-    const USAGE: &str = "
-Usage: osm4routing <source.osm.pbf> [--format=<output_format>]
+
+use osm4routing::{osm_read, csv_read, writers, Node, Edge};
+
+const USAGE: &str = "
+Usage: osm4routing [--format=<output_format>] (<source.osm.pbf> | <nodes.csv> <ways.csv>)
 Options:
     --format=<output_format>  Output format (csv or geojson) [default: csv]";
 
+fn main() {
     let args = docopt::Docopt::new(USAGE)
         .unwrap()
         .parse()
         .unwrap_or_else(|e| e.exit());
-    let filename = args.get_str("<source.osm.pbf>");
 
-    match osm4routing::read(filename) {
-        Ok((nodes, edges)) => {
-            let fmt = args.get_str("--format");
-            match fmt {
-                "csv" | "" => osm4routing::writers::csv(nodes, edges),
-                "geojson" => osm4routing::writers::geojson(nodes, edges),
-                _ => println!("Invalid output format. Please choose csv or geojson."),
-            }
-        },
-        Err(error) => println!("Error: {}", error),
+    let fmt = args.get_str("--format");
+
+    if args.get_bool("<source.osm.pbf>") {
+        match osm_read(args.get_str("<source.osm.pbf>")) {
+            Ok((nodes, edges)) => handle_output_format(fmt, nodes, edges),
+            Err(e) => println!("Error: {}", e),
+        }
+    } else {
+        match csv_read(args.get_str("<nodes.csv>"), args.get_str("<ways.csv>"))  {
+            Ok((nodes, edges)) => handle_output_format(fmt, nodes, edges),
+            Err(e) => println!("Error: {}", e),
+        }
+    }
+}
+
+fn handle_output_format(fmt: &str, nodes: Vec<Node>, edges: Vec<Edge>) {
+    match fmt {
+        "csv" | "" => writers::csv(nodes, edges),
+        "geojson" => writers::geojson(nodes, edges),
+        _ => println!("Invalid output format. Please choose csv or geojson."),
     }
 }
