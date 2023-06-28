b7023107308616165672dc20e1648d6b4448308a
Capucinoxx <coub2802@usherbrooke.ca>
2023-06-28 11:28:23 -0400

    chore: reader lib deps

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,5 @@ pub use crate::osm4routing::categorize::{
     BikeAccessibility, CarAccessibility, FootAccessibility, TrainAccessibility,
 };
 pub use crate::osm4routing::models::*;
-pub use crate::osm4routing::reader::{read, Reader};
+pub use crate::osm4routing::reader::{osm_read, OsmReader, csv_read, CsvReader};
 pub use crate::osm4routing::writers;
