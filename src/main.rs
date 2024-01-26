3491285b6a6f56d3ee473992588645acc90c61b4
Tristram Gräbener <tristram+git@tristramg.eu>
2024-01-26 11:37:58 +0100

    Add command line parameters to specify output files

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,22 @@ use clap::Parser;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
+    /// Input OpenStreetMap in the .pbf format
     source_pbf: String,
+    /// Output path of the csv file that will contain the nodes
+    #[arg(short, long, default_value = "nodes.csv")]
+    nodes_file: String,
+    /// Output path of the csv file that will contain the edges
+    #[arg(short, long, default_value = "nodes.csv")]
+    edges_file: String,
 }
 fn main() {
     let cli = Cli::parse();
 
     match osm4routing::read(&cli.source_pbf) {
-        Ok((nodes, edges)) => osm4routing::writers::csv(nodes, edges),
+        Ok((nodes, edges)) => {
+            osm4routing::writers::csv(nodes, edges, &cli.nodes_file, &cli.edges_file)
+        }
         Err(error) => println!("Error: {}", error),
     }
 }
