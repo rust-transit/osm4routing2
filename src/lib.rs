dd5918effd78edc3e9bdbbf412ee020282e9e742
Jean SIMARD <woshilapin@tuziwo.info>
2025-04-11 15:47:54 +0200

    feat: reexpose osmpbfreader and ahash

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,8 @@ pub use crate::osm4routing::categorize::{
 pub use crate::osm4routing::models::*;
 pub use crate::osm4routing::reader::{read, Reader};
 pub use crate::osm4routing::writers;
+
+// Reexpose crates that are part of the API
+// It helps consumer being sure they always use the correct version of types
+pub use ahash;
+pub use osmpbfreader;
