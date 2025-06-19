21e1812c7de403486e9c3a80aa88a0530af961a7
gvsyn <projects@lostnode.org>
2025-06-19 12:18:54 +0200

    Fix up table creation (#55)
    The column count between the CSV and the tables did not match.
    The enum is an aggregate of all the modes, which is good enough
    for getting things working.

--- create_tables.sql
+++ create_tables.sql
@@ -1,5 +1,9 @@
 DROP TABLE IF EXISTS edges;
 DROP TABLE IF EXISTS nodes;
+DROP TYPE IF EXISTS accessibility;
+
+-- Could be seperated into multiple enum types, this was a quick enough fix
+CREATE TYPE accessibility AS ENUM ('Unknown', 'Forbidden', 'Allowed', 'Residential', 'Tertiary', 'Secondary', 'Primary', 'Trunk', 'Motorway', 'Lane', 'Busway', 'Track');
 
 CREATE TABLE nodes (
     id BIGINT PRIMARY KEY,
@@ -7,16 +11,20 @@ CREATE TABLE nodes (
     latitude DOUBLE PRECISION
 );
 
+--id,osm_id,source,target,length,foot,car_forward,car_backward,bike_forward,bike_backward,train,wkt
+
 CREATE TABLE edges (
-    id BIGINT,
+    id TEXT,
+	osm_id BIGINT,
     source BIGINT REFERENCES nodes(id),
     target BIGINT REFERENCES nodes(id),
     length REAL,
-    foot INT,
-    car_forward INT,
-    car_backward INT,
-    bike_forward INT,
-    bike_backward INT,
+    foot accessibility,
+    car_forward accessibility,
+    car_backward accessibility,
+    bike_forward accessibility,
+    bike_backward accessibility,
+	train accessibility,
     wkt TEXT
 );
 
